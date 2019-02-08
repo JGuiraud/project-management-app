f53274b3fca1c352e8870399db31bba6b31ce30c
Jérôme Guiraud <jerome31guiraud@gmail.com>
2019-02-08 12:10:54 +0100

    details and grabbing id param from url

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,1 +1,25 @@
+import React from 'react';
 
+const ProjectDetails = (props) => {
+  const id = props.match.params.id;
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title - {id}</span>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid sint tempore totam. Ad cupiditate ex hic molestiae mollitia neque, nihil officiis porro similique velit! Accusantium consequuntur harum nesciunt omnis perferendis.</p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>
+            Posted by JG
+          </div>
+          <div>
+            08/02/19 à 11h
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return(
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content frey-text text-darken-3">
+          <span className="card-title">
+            Project Title
+          </span>
+        <p>Posted by JG</p>
+        <p className="grey-text">08/02/19, 11h</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary;
