2cbc353eda09a8dd614ba39077efa951e175ea91
Jérôme Guiraud <jerome31guiraud@gmail.com>
2019-02-08 11:01:05 +0100

    links in navbar, dashboard with project lists and notifications

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import NavBar from './components/layout/NavBar';
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>Project Management Tool</h1>
+          <NavBar />
+          <Switch>
+            <Route path="/" component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const ProjectList = () => {
+  return(
+    <div className="project-list section">
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content frey-text text-darken-3">
+          <span className="card-title">
+            Project Title
+          </span>
+          <p>Posted by JG</p>
+          <p className="grey-text">08/02/19, 11h</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content frey-text text-darken-3">
+          <span className="card-title">
+            Project Title
+          </span>
+          <p>Posted by JG</p>
+          <p className="grey-text">08/02/19, 11h</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content frey-text text-darken-3">
+          <span className="card-title">
+            Project Title
+          </span>
+          <p>Posted by JG</p>
+          <p className="grey-text">08/02/19, 11h</p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default ProjectList;
